0af13a52f0be7cb1350479cf14b23776861dc54e
Kovalenko Vladislav <vkoval2002@gmail.com>
2020-07-07 22:59:39 +0300

    DrawController reworked

--- src/controllers/DrawController.java
+++ src/controllers/DrawController.java
@@ -1,8 +1,10 @@
 package controllers;
 
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Group;
+import javafx.scene.Node;
 import javafx.scene.control.TextField;
 import javafx.scene.effect.Light;
 import javafx.scene.layout.AnchorPane;
@@ -10,6 +12,8 @@ import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Rectangle;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class DrawController {
@@ -23,6 +27,10 @@ public class DrawController {
 
     @FXML
     private TextField piValueTextField;
+
+    private static void run() {
+
+    }
 
     public void setupDrawScene(MainApp mainApp, AnchorPane drawAnchorPane, int dotsCount) {
         DrawController.mainApp = mainApp;
@@ -41,26 +49,49 @@ public class DrawController {
 
     @FXML
     private Circle paneCircle;
+    private boolean isRunning;
+    private int circlePoints = 0;
+    private int totalPoints = 0;
+
+    void addDot() {
+        double x = ThreadLocalRandom.current().nextInt(-R, R);
+        double y = ThreadLocalRandom.current().nextInt(-R, R);
+        totalPoints++;
+        Circle dot = new Circle(paneCircle.getCenterX() + x, paneCircle.getCenterY() + y, 1, null);
+        if (Math.sqrt(x * x + y * y) < R) {
+            circlePoints++;
+            dot.setFill(Color.rgb(0, 191, 255));
+        } else
+            dot.setFill(Color.rgb(255, 97, 223));
+        drawAnchorPane.getChildren().add(dot);
+        float pi = (float) 4.0 * circlePoints / totalPoints;
+        piValueTextField.setText(Double.toString(pi));
+    }
 
     @FXML
     void onStartButtonPressed() {
-        int circlePoints = 0;
-        int totalPoints = 0;
-        
-        for (int i = 0; i < dotsCount; i++) {
-            double x = ThreadLocalRandom.current().nextInt(-R, R);
-            double y = ThreadLocalRandom.current().nextInt(-R, R);
-            totalPoints++;
-            Circle dot = new Circle(paneCircle.getCenterX() + x, paneCircle.getCenterY() + y, 1, null);
-            if (Math.sqrt(x * x + y * y) < R) {
-                circlePoints++;
-                dot.setFill(Color.rgb(0,191,255));
-            } else
-                dot.setFill(Color.rgb(255,97,223));
-            drawAnchorPane.getChildren().add(dot);
-            dotsCountTextField.setText(Integer.toString(i));
-        }
-        float pi = (float) 4.0 * circlePoints / totalPoints;
-        piValueTextField.setText(Double.toString(pi));
+        Platform.runLater(() -> {
+            long desiredFrameRateTime = 1000 / 60; //желаемое количетсво кадров в секунду (60 fps)
+            long currentTime = System.currentTimeMillis();
+            long lastTime = currentTime - desiredFrameRateTime;
+            long unprocessedTime = 0;        //необработанное время
+            int i = 0;
+            while (i <= dotsCount) {
+                currentTime = System.currentTimeMillis();  //текущее время
+                unprocessedTime += currentTime - lastTime; //время прошедшее без обработки
+                lastTime = currentTime;
+                while (unprocessedTime >= desiredFrameRateTime) {
+                    unprocessedTime -= desiredFrameRateTime;
+                }
+                addDot();
+                /*try {
+                    Thread.sleep(20);
+                } catch (InterruptedException exception) {
+                    exception.printStackTrace();
+                }*/
+                dotsCountTextField.setText(Integer.toString(i));
+                i++;
+            }
+        });
     }
 }
